de3b7d05f84e60d5801b970a3275d9395c556511
vedant <vedantguptadsa@gmail.com>
2026-04-22 01:20:29 +0530

    comment update

--- AI_Investment_System_Backend/src/main/java/com/investment/investment_system/InvestmentSystemApplication.java
+++ AI_Investment_System_Backend/src/main/java/com/investment/investment_system/InvestmentSystemApplication.java
@@ -3,7 +3,8 @@ package com.investment.investment_system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //scans com.investment.investment_system.* for components, configurations, and services
+
 public class InvestmentSystemApplication {
 
 	public static void main(String[] args) {
