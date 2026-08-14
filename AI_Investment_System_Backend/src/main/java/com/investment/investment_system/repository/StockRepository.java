089d17d2fcd8bd72f273b1de6105b67623cfde62
vedant <vedantguptadsa@gmail.com>
2026-08-14 18:07:56 +0530

    Update StockRepository.java

--- AI_Investment_System_Backend/src/main/java/com/investment/investment_system/repository/StockRepository.java
+++ AI_Investment_System_Backend/src/main/java/com/investment/investment_system/repository/StockRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface StockRepository extends JpaRepository<Stock, Long> {
+public interface StockRepository extends JpaRepository<Stock, Long> { //Create a repository for the Stock entity, extending JpaRepository to provide CRUD operations and additional query methods.
 
     Optional<Stock> findBySymbol(String symbol);
 }
