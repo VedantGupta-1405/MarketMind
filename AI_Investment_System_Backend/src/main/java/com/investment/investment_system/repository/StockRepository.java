034e397459d4b412cc64b66f183bdf55b3fcb857
vedant <vedantguptadsa@gmail.com>
2026-08-14 18:10:49 +0530

    Update StockRepository.java

--- AI_Investment_System_Backend/src/main/java/com/investment/investment_system/repository/StockRepository.java
+++ AI_Investment_System_Backend/src/main/java/com/investment/investment_system/repository/StockRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface StockRepository extends JpaRepository<Stock, Long> { //Create a repository for the Stock entity, extending JpaRepository to provide CRUD operations and additional query methods.
 
-    Optional<Stock> findBySymbol(String symbol);
+    Optional<Stock> findBySymbol(String symbol); // this method allows you to find a Stock entity by its symbol, returning an Optional that may or may not contain a Stock object.
 }
