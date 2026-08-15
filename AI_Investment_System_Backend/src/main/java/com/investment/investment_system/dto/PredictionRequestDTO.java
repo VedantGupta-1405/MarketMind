56da3c0398fcaecfeed087ce04d22ab50475ad0a
vedant <vedantguptadsa@gmail.com>
2026-08-15 17:19:27 +0530

    Update PredictionRequestDTO.java

--- AI_Investment_System_Backend/src/main/java/com/investment/investment_system/dto/PredictionRequestDTO.java
+++ AI_Investment_System_Backend/src/main/java/com/investment/investment_system/dto/PredictionRequestDTO.java
@@ -3,6 +3,8 @@ package com.investment.investment_system.dto;
 public class PredictionRequestDTO {
 
     private Long stockId;
+    private String newsTitle;
+    private String newsContent;
 
     public PredictionRequestDTO() {}
 
@@ -12,5 +14,21 @@ public class PredictionRequestDTO {
 
     public void setStockId(Long stockId) {
         this.stockId = stockId;
+    }
+
+    public String getNewsTitle() {
+        return newsTitle;
+    }
+
+    public void setNewsTitle(String newsTitle) {
+        this.newsTitle = newsTitle;
+    }
+
+    public String getNewsContent() {
+        return newsContent;
+    }
+
+    public void setNewsContent(String newsContent) {
+        this.newsContent = newsContent;
     }
 }
