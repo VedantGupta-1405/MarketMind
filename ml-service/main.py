34b9c0b3d2f3b9456aa72dda9ab68d2d61ed6499
vedant <vedantguptadsa@gmail.com>
2026-08-15 17:11:43 +0530

    Update main.py

--- ml-service/main.py
+++ ml-service/main.py
@@ -4,22 +4,33 @@ from model import predict_stock
 
 app = FastAPI()
 
+
 class PredictionRequest(BaseModel):
     stockId: int
+    newsTitle: str = ""
+    newsContent: str = ""
+
 
 class PredictionResponse(BaseModel):
     prediction: str
     probability: float
 
+
 @app.get("/")
 def root():
     return {"message": "ML Service Running"}
 
+
 @app.post("/predict", response_model=PredictionResponse)
 def predict(request: PredictionRequest):
-    prediction, probability = predict_stock(request.stockId)
+
+    prediction, probability = predict_stock(
+        request.stockId,
+        request.newsTitle,
+        request.newsContent
+    )
 
     return PredictionResponse(
         prediction=prediction,
         probability=probability
-    )   
+    )
