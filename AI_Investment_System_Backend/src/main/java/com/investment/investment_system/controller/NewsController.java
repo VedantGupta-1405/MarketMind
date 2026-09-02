fd42593a1ada3d5a40ec07d0a38784186da3b6cd
vedant <vedantguptadsa@gmail.com>
2026-09-02 10:40:24 +0530

    Update NewsController.java

--- AI_Investment_System_Backend/src/main/java/com/investment/investment_system/controller/NewsController.java
+++ AI_Investment_System_Backend/src/main/java/com/investment/investment_system/controller/NewsController.java
@@ -2,11 +2,13 @@ package com.investment.investment_system.controller;
 
 import com.investment.investment_system.entity.News;
 import com.investment.investment_system.service.NewsService;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/news")
 public class NewsController {
@@ -22,11 +24,17 @@ public class NewsController {
             @PathVariable Long stockId,
             @RequestBody News news
     ) {
-        return ResponseEntity.ok(newsService.addNews(stockId, news));
+        return ResponseEntity.ok(
+                newsService.addNews(stockId, news)
+        );
     }
 
     @GetMapping("/{stockId}")
-    public ResponseEntity<List<News>> getNews(@PathVariable Long stockId) {
-        return ResponseEntity.ok(newsService.getNews(stockId));
+    public ResponseEntity<List<News>> getNews(
+            @PathVariable Long stockId
+    ) {
+        return ResponseEntity.ok(
+                newsService.getNews(stockId)
+        );
     }
 }
